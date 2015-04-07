14580e46a10b3bc6409a52d985c24ab08a8fea6e
sumtopmus <sumtopmus@users.noreply.github.com>
2015-04-07 00:05:10 -0400

    Lecture 5 - done

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Oleksandr Iaroshenko on 19.03.15.
+//  Copyright (c) 2015 Oleksandr Iaroshenko. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
